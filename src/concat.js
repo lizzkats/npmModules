9d3cb25a5589566dd62a6b511fbf8ab313f83ba9
lizzkats <katsnelson.lizz@gmail.com>
2016-12-23 17:37:44 -0800

    Adds a better concat and better test cases

--- src/concat.js
+++ src/concat.js
@@ -1,9 +1,19 @@
-const concat = (...args) => {
-  const arguments = [...args]
 
-  return arguments.reduce((str, str2) => {
-    return str + str2
-  }, '')
+//iterate over all arguments except the first one
+const concat = function () {
+//loop over the arguments and pull out every element of each array and append each value to the end of the original array
+//return a modified version of the original array
+  let originalArray = arguments[0]
+  for (let i = 0; i < arguments.length - 1; i++) {
+    let currentArgument = arguments[i+1]
+    for (let j = 0; j < currentArgument.length; j++) {
+      originalArray.push(currentArgument[j])
+    }
+  }
+  return originalArray
 }
 
-module.exports = concat
+
+
+
+module.exports = concat;
